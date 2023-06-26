d28d31f81403a22796c113218747e0c409348023
Albet Novendo <76980465+albetnov@users.noreply.github.com>
2023-06-26 23:21:52 +0700

    Delete src directory

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import useEditor from 'lib/compositions/useEditor'
-
-const { editor } = useEditor()
-</script>
-
-<template>
-  <vue-editor :config="editor" />
-</template>
